92571b62bcf29a9bb7833956764a8e6c1b225bc4
quietdreamr <kidfabi.97@gmail.com>
2023-04-16 14:39:38 +0200

    exampleInputEmail1 -> user_id

--- public/js/login.js
+++ public/js/login.js
@@ -2,13 +2,13 @@ function store() {
     // get first name
     let inputExampleInputEmail1 = document.getElementById("exampleInputEmail1");
     // save first name
-    localStorage.setItem("exampleInputEmail1", inputExampleInputEmail1.value);
+    localStorage.setItem("user_id", inputExampleInputEmail1.value);
     
   }
      
       //document.body.onload = addUserName(); // attaching the event handler- addUserName function​​
       function addUserName() {
-      const userName = localStorage.exampleInputEmail1;
+      const userName = localStorage.user_id;
        if (userName !== undefined) {
          document.getElementById("messagename").innerHTML =
          "Welcome, " + userName;
